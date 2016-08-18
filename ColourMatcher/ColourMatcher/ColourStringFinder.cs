d0993e64e41a87a2257dcc6ee6447bc9777e3af8
Brian Jones <brian.jones@cap.co.uk>
2016-08-18 14:25:01 +0100

    initial update

---
+++ ColourMatcher/ColourMatcher/ColourStringFinder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace ColourMatcher
+    {
+    public class ColourStringfinder
+    {
+        private double minTemp { get; set; } = -3.9;
+        private double maxTemp { get; set; } = 40.3;
+        ///<summary>
+        ///Takes a double representing the current temperature and returns a colour representing that colour
+        ///</summary>
+        ///<param name ="temp">The temperature to represent as a colour</param>
+        public Color getColour (double temp)
+            {
+            Color cold = Color.RoyalBlue;
+            Color hot = Color.Red;
+            double ratio = (temp - minTemp) / (maxTemp - minTemp);
+            int R = (byte)Math.Min((Math.Round(cold.R + ratio * (hot.R - cold.R))),255);
+            int G = (byte)Math.Min((Math.Round(ratio * (1 - ratio) * byte.MaxValue * 4)),255); //Green pixel used to brighten middle values
+            int B = (byte)Math.Min((Math.Round(cold.B + ratio * (hot.B - cold.B))),255);
+            Color result = Color.FromArgb(R, G, B);
+            return result;
+            }
+
+
+        ///<summary>
+        ///Takes a string representing a temperature, and represents an associated colour in a colour hex string for web use.
+        ///</sumary>
+        ///<param name ="temp">The temperature to represent as a colour</param>
+        public String getColourHash (String temp)
+            {
+            try
+                {
+                double dtemp = double.Parse(temp);
+                Color result = getColour(dtemp);
+                return (String.Format(@"#{0:X2}{1:X2}{2:X2}", result.R, result.G, result.B));
+                }
+            catch (FormatException e)
+                {
+                return (@"#cccccc;");
+                }
+            catch (Exception e)
+                {
+                Debug.WriteLine(e.Message);
+                return (@"#cccccc;");
+                }
+            }
+        }
+}
---
+++ ColourMatcher/ColourStringTests/ColourStringTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using ColourMatcher;
+
+namespace ColourStringTests
+    {
+    [TestClass]
+    public class ColourStringTests
+        {
+        //testvalue Dictionary - Keys represent test Temperatures, Values represent colours
+        Dictionary<string, string> TestValues = new Dictionary<string, string>()
+            {
+                {"-4", "#41FEE2" }, //Under expected Range
+                {"-3","#4514DC" },
+                {"0", "#5252CD" },
+                {"4", "#6396B9" },
+                {"8", "#74C9A4" },
+                {"12", "#85EB90" },
+                {"16", "#97FC7C" },
+                {"20", "#A8FD67" },
+                {"24", "#B9ED53" },
+                {"28", "#CACD3F" },
+                {"32", "#DB9C2A" },
+                {"36", "#ED5A16" },
+                {"40", "#FE0702" },
+                {"44", "#FFA3ED" }//Over expected range
+            };
+
+        //matching function - matches if R and B values match exactly.  Not worried about Green, or alpha Values
+        private bool colourMatch (Color colour1, Color colour2)
+            {
+            var redmatch = colour1.R == colour2.R;
+            var bluematch = colour1.B == colour2.B;
+            return redmatch && bluematch;
+            }
+        /// <summary>
+        /// The top and bottom colours should yeild the predictable colours
+        /// </summary>
+        [TestMethod]
+        public void TestLimits ()
+            {
+            //testvalue Dictionary - Keys represent test Temperatures, Values represent colours
+            Dictionary<double, Color> TestValues = new Dictionary<double, Color>()
+            {
+                    {-3.9, Color.RoyalBlue },
+                    {40.3, Color.Red }
+            };
+            foreach (var temp in TestValues)
+                {
+                ColourStringfinder csf = new ColourStringfinder();
+                Assert.IsTrue(colourMatch(csf.getColour(temp.Key), temp.Value), $"Colours do not match for Temprature {temp.Key}, Got {csf.getColour(temp.Key)}, but expected {temp.Value}");
+                }
+            }
+
+        [TestMethod]
+        public void SampleValuesIntoStrings ()
+            {
+            
+            foreach (var temp in TestValues)
+                {
+                ColourStringfinder csf = new ColourStringfinder();
+
+                Assert.AreEqual(temp.Value, csf.getColourHash(temp.Key), $"Colours do not match for Temperature {temp.Key}, Got {csf.getColourHash(temp.Key)}, but expected {temp.Value}");
+                }
+            }
+
+        [TestMethod]
+        public void GenerateHtmlTableInDebug ()
+            {
+            
+            Debug.WriteLine("<table><tr><th>Temperature (C)</th><th>Hash Code</th></tr>");
+            foreach (var temp in TestValues)
+                {
+                ColourStringfinder csf = new ColourStringfinder();
+                Debug.WriteLine($"<tr bgcolor={temp.Value}><td>{temp.Key}</td><td>{temp.Value}</td></tr>");
+                }
+            Debug.WriteLine("</table>");
+            }
+
+        }
+    }
